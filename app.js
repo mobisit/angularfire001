3b8bc444d89b8a958fa50a212f0a41ce743bb2e1
Peter Choi <workaholic@192-168-1-7.tpgi.com.au>
2015-09-02 21:57:24 +1000

    Initial commit
    This is first app created using AngularJS and Firebase

---
+++ app.js
@@ -0,0 +1,45 @@
+// create our angular module and inject firebase
+angular.module('scheduleApp', ['firebase'])
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+  // our application code will go here
+  // connect to firebase
+  var ref = new Firebase("https://angularfire001.firebaseio.com/days");
+  var fb = $firebase(ref);
+  // sync as object
+  var syncObject = fb.$asObject();
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
+
+  // function to set the default data
+  $scope.reset = function(){
+  	fb.$set({
+  		monday: {
+  			name: 'Monday',
+  			slots: {
+  				0900: {
+  					time: '9:00am',
+  					booked: false
+  				},
+  				1100: {
+  					time: '11:00am',
+  					booked: false
+  				}
+  			}
+  		},
+  		tuesday: {
+  			name: 'Tuesday',
+  			slots: {
+  				0900: {
+  					time: '9:00am',
+  					booked: false
+  				},
+  				1100: {
+  					time: '11:00am',
+  					booked: false
+  				}
+  			}
+  		}
+  	});
+  };
+});
