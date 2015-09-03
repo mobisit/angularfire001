da830e0f7714003d1b532f4282546761a27ef6b7
Peter Choi <workaholic@192-168-1-7.tpgi.com.au>
2015-09-03 23:21:58 +1000

    update v1.01.01
    Updated angularJS and firebase versions and made them local

--- app.js
+++ app.js
@@ -1,44 +1,218 @@
 // create our angular module and inject firebase
 angular.module('scheduleApp', ['firebase'])
 // create our main controller and get access to firebase
-.controller('mainController', function($scope, $firebase) {
+.controller('mainController', function($scope, $firebaseObject) {
   // our application code will go here
   // connect to firebase
   var ref = new Firebase("https://angularfire001.firebaseio.com/days");
-  var fb = $firebase(ref);
+  //var fb = $firebase(ref);
   // sync as object
-  var syncObject = fb.$asObject();
+  var syncObject = $firebaseObject(ref);
   // three way data binding
   syncObject.$bindTo($scope, 'days');
 
   // function to set the default data
   $scope.reset = function(){
-  	fb.$set({
+  	ref.set({
   		monday: {
   			name: 'Monday',
   			slots: {
   				0900: {
   					time: '9:00am',
-  					booked: false
+  					booked: false,
+            hidden: false
   				},
-  				1100: {
-  					time: '11:00am',
-  					booked: false
-  				}
+  				1000: {
+  					time: '10:00am',
+  					booked: false,
+            hidden: false
+  				},
+          1100: {
+            time: '11:00am',
+            booked: false,
+            hidden: false
+          },
+          1200: {
+            time: '12:00pm',
+            booked: false,
+            hidden: false
+          },
+          1300: {
+            time: '1:00pm',
+            booked: false,
+            hidden: false
+          },
+          1400: {
+            time: '2:00pm',
+            booked: false,
+            hidden: false
+          },
+          1500: {
+            time: '3:00pm',
+            booked: false,
+            hidden: false
+          }
   			}
   		},
-  		tuesday: {
-  			name: 'Tuesday',
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false,
+            hidden: false
+          },
+          1000: {
+            time: '10:00am',
+            booked: false,
+            hidden: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false,
+            hidden: false
+          },
+          1200: {
+            time: '12:00pm',
+            booked: false,
+            hidden: false
+          },
+          1300: {
+            time: '1:00pm',
+            booked: false,
+            hidden: false
+          },
+          1400: {
+            time: '2:00pm',
+            booked: false,
+            hidden: false
+          },
+          1500: {
+            time: '3:00pm',
+            booked: false,
+            hidden: false
+          }
+        }
+      },
+      wednesday: {
+        name: 'Wednesday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false,
+            hidden: false
+          },
+          1000: {
+            time: '10:00am',
+            booked: false,
+            hidden: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false,
+            hidden: false
+          },
+          1200: {
+            time: '12:00pm',
+            booked: false,
+            hidden: false
+          },
+          1300: {
+            time: '1:00pm',
+            booked: false,
+            hidden: false
+          },
+          1400: {
+            time: '2:00pm',
+            booked: false,
+            hidden: false
+          },
+          1500: {
+            time: '3:00pm',
+            booked: false,
+            hidden: false
+          }
+        }
+      },
+      thursday: {
+        name: 'Thursday',
+        slots: {
+          0900: {
+            time: '9:00am',
+            booked: false,
+            hidden: false
+          },
+          1000: {
+            time: '10:00am',
+            booked: false,
+            hidden: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false,
+            hidden: false
+          },
+          1200: {
+            time: '12:00pm',
+            booked: false,
+            hidden: false
+          },
+          1300: {
+            time: '1:00pm',
+            booked: false,
+            hidden: false
+          },
+          1400: {
+            time: '2:00pm',
+            booked: false,
+            hidden: false
+          },
+          1500: {
+            time: '3:00pm',
+            booked: false,
+            hidden: false
+          }
+        }
+      },
+  		friday: {
+  			name: 'Friday',
   			slots: {
-  				0900: {
-  					time: '9:00am',
-  					booked: false
-  				},
-  				1100: {
-  					time: '11:00am',
-  					booked: false
-  				}
-  			}
+          0900: {
+            time: '9:00am',
+            booked: false,
+            hidden: false
+          },
+          1000: {
+            time: '10:00am',
+            booked: false,
+            hidden: false
+          },
+          1100: {
+            time: '11:00am',
+            booked: false,
+            hidden: false
+          },
+          1200: {
+            time: '12:00pm',
+            booked: false,
+            hidden: false
+          },
+          1300: {
+            time: '1:00pm',
+            booked: false,
+            hidden: false
+          },
+          1400: {
+            time: '2:00pm',
+            booked: false,
+            hidden: false
+          },
+          1500: {
+            time: '3:00pm',
+            booked: false,
+            hidden: false
+          }
+        }
   		}
   	});
   };
